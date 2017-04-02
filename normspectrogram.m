e426c3d25ac5904ddd93074bf8b9a1be9829350f
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-04-01 23:53:19 -0300

    Added some vector check

--- normspectrogram.m
+++ normspectrogram.m
@@ -30,7 +30,32 @@ function matrix = normspectrogram(t, acc)
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+%% Constantes
+WINDOW_TIME = 5;
+WINDOW_TIME_MOVEMENT = 2.5;
 
+%% Check that length of arrays is the same
+tlen = length(t);
+acclen = length(acc);
+if tlen ~= acclen
+    error('Length of t and acc vectors must be the same');
+end
+
+%% Check that length of arrays are greather than 2
+if tlen < 2
+    error('Length of t and cc vector must be greater than 2');
+end
+
+%% Calculate dt
+dt = t(2)-t(1)
+
+%% Check that vector length are greather
+
+%% Create 5-seconds windows overlapped by 2.5 seconds
+t_ini = 0; % Initial time
+while true
+    
+end
 
 end
 
